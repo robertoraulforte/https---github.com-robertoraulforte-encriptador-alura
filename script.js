c1b03dc05b454c0fa2f3b5fa85f66e42b94c269d
Roberto <robertoraulforte@gmail.com>
2022-08-22 19:10:25 -0300

    primera version

---
+++ script.js
@@ -0,0 +1,17 @@
+const inputTexto = document.querySelector(".input-texto")
+const mensaje = document.querySelector(".mensaje")
+
+//La letra "e" es convertida para "enter"
+//La letra "i" es convertida para "imes"
+//La letra "a" es convertida para "ai"
+//La letra "o" es convertida para "ober"
+//La letra "u" es convertida para "ufat"
+
+
+function encriptar(stringEncriptada){
+    let matrizCodigo [[ "e","enter"], []]
+
+    stringEncriptada = stringEncriptada.toLowerCase();
+    
+
+}
